af39533f8e0fea1db8f67c0c9ad537f06c65725a
Mia Azevedo <mialuzazevedo@gmail.com>
2024-03-06 21:56:45 -0400

    creates ninja-jobs project

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, cumque reiciendis veritatis minus eveniet fugit saepe quam voluptas, officia nobis magni, perspiciatis ipsa nam mollitia? Saepe, rerum voluptatum nihil repellendus esse fuga a sit dolor mollitia omnis quibusdam recusandae! Amet iusto ex id aut facilis, optio vero culpa laudantium! Magni labore corrupti est laudantium, dolorem totam excepturi! Fuga perspiciatis dolorum maxime! Nobis harum voluptate quis? Minus itaque voluptas nihil cumque exercitationem sunt nemo pariatur vero suscipit officia non asperiores sint optio maxime, cupiditate deserunt, dolore blanditiis est deleniti neque iusto magnam dolorum! Possimus minus nulla distinctio alias doloribus vero asperiores.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda, cumque reiciendis veritatis minus eveniet fugit saepe quam voluptas, officia nobis magni, perspiciatis ipsa nam mollitia? Saepe, rerum voluptatum nihil repellendus esse fuga a sit dolor mollitia omnis quibusdam recusandae! Amet iusto ex id aut facilis, optio vero culpa laudantium! Magni labore corrupti est laudantium, dolorem totam excepturi! Fuga perspiciatis dolorum maxime! Nobis harum voluptate quis? Minus itaque voluptas nihil cumque exercitationem sunt nemo pariatur vero suscipit officia non asperiores sint optio maxime, cupiditate deserunt, dolore blanditiis est deleniti neque iusto magnam dolorum! Possimus minus nulla distinctio alias doloribus vero asperiores.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {  },
+}
+</script>
